ca665543ac26932a7528c4995eb9dc963bd55a0a
Mateusz Dukat <mateusz.dukat1@gmail.com>
2020-11-17 19:52:41 +0100

    Main function up to the firewall

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -610,6 +610,99 @@ else
 	esac
 	else
 		# OpenBSD_PORT - here make all of this stuff, but on OpenBSD
-		
+		mkdir -p /etc/openvpn/server/easy-rsa/
+		chown -R root:root /etc/openvpn/server/easy-rsa/
+		cd /etc/openvpn/server/easy-rsa/
+		# Create the PKI, set up the CA and the server and client certificates
+		/usr/local/share/easy-rsa/easyrsa init-pki
+		/usr/local/share/easy-rsa/easyrsa --batch build-ca nopass
+		EASYRSA_CERT_EXPIRE=3650 /usr/local/share/easy-rsa/easyrsa build-server-full server nopass
+		EASYRSA_CERT_EXPIRE=3650 /usr/local/share/easy-rsa/easyrsa build-client-full "$client" nopass
+		EASYRSA_CRL_DAYS=3650 /usr/local/share/easy-rsa/easyrsa gen-crl
+		# Move the stuff we need
+		cp pki/ca.crt pki/private/ca.key pki/issued/server.crt pki/private/server.key pki/crl.pem /etc/openvpn/server
+		# CRL is read with each client connection, while OpenVPN is dropped to nobody
+		chown nobody:"$group_name" /etc/openvpn/server/crl.pem
+		# Without +x in the directory, OpenVPN can't run a stat() on the CRL file
+		chmod o+x /etc/openvpn/server/
+		# Generate key for tls-crypt
+		openvpn --genkey --secret /etc/openvpn/server/tc.key
+		# Create the DH parameters file using the predefined ffdhe2048 group
+		echo '-----BEGIN DH PARAMETERS-----
+MIIBCAKCAQEA//////////+t+FRYortKmq/cViAnPTzx2LnFg84tNpWp4TZBFGQz
++8yTnc4kmz75fS/jY2MMddj2gbICrsRhetPfHtXV/WVhJDP1H18GbtCFY2VVPe0a
+87VXE15/V8k1mE8McODmi3fipona8+/och3xWKE2rec1MKzKT0g6eXq8CrGCsyT7
+YdEIqUuyyOP7uWrat2DX9GgdT0Kj3jlN9K5W7edjcrsZCwenyO4KbXCeAvzhzffi
+7MA0BM0oNC9hkXL+nOmFg/+OTxIy7vKBg8P+OxtMb61zO7X8vC7CIAXFjvGDfRaD
+ssbzSibBsu/6iGtCOGEoXJf//////////wIBAg==
+-----END DH PARAMETERS-----' > /etc/openvpn/server/dh.pem
+		# Generate server.conf
+		echo "local $ip
+port $port
+proto $protocol
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh.pem
+auth SHA512
+tls-crypt tc.key
+topology subnet
+server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
+		# IPv6
+		if [[ -z "$ip6" ]]; then
+			echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server/server.conf
+		else
+			echo 'server-ipv6 fddd:1194:1194:1194::/64' >> /etc/openvpn/server/server.conf
+			echo 'push "redirect-gateway def1 ipv6 bypass-dhcp"' >> /etc/openvpn/server/server.conf
+		fi
+		echo 'ifconfig-pool-persist ipp.txt' >> /etc/openvpn/server/server.conf
+		# DNS
+		case "$dns" in
+			1|"")
+				resolv_conf="/etc/resolv.conf"
+				# Obtain the resolvers from resolv.conf and use them for OpenVPN
+				grep -v '^#\|^;' "$resolv_conf" | grep '^nameserver' | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | while read line; do
+					echo "push \"dhcp-option DNS $line\"" >> /etc/openvpn/server/server.conf
+				done
+			;;
+			2)
+				echo 'push "dhcp-option DNS 8.8.8.8"' >> /etc/openvpn/server/server.conf
+				echo 'push "dhcp-option DNS 8.8.4.4"' >> /etc/openvpn/server/server.conf
+			;;
+			3)
+				echo 'push "dhcp-option DNS 1.1.1.1"' >> /etc/openvpn/server/server.conf
+				echo 'push "dhcp-option DNS 1.0.0.1"' >> /etc/openvpn/server/server.conf
+			;;
+			4)
+				echo 'push "dhcp-option DNS 208.67.222.222"' >> /etc/openvpn/server/server.conf
+				echo 'push "dhcp-option DNS 208.67.220.220"' >> /etc/openvpn/server/server.conf
+			;;
+			5)
+				echo 'push "dhcp-option DNS 9.9.9.9"' >> /etc/openvpn/server/server.conf
+				echo 'push "dhcp-option DNS 149.112.112.112"' >> /etc/openvpn/server/server.conf
+			;;
+			6)
+				echo 'push "dhcp-option DNS 94.140.14.14"' >> /etc/openvpn/server/server.conf
+				echo 'push "dhcp-option DNS 94.140.15.15"' >> /etc/openvpn/server/server.conf
+			;;
+		esac
+		echo "keepalive 10 120
+cipher AES-256-CBC
+user nobody
+group $group_name
+persist-key
+persist-tun
+status openvpn-status.log
+verb 3
+crl-verify crl.pem" >> /etc/openvpn/server/server.conf
+		if [[ "$protocol" = "udp" ]]; then
+			echo "explicit-exit-notify" >> /etc/openvpn/server/server.conf
+		fi
+		# Enable net.ipv4.ip_forward for the system
+		echo 'net.inet.ip.forwarding=1' >> /etc/sysctl.conf
+		sysctl net.inet.ip.forwarding=1
+		# Firewall
+		# OpenBSD_PORT - line 339 at https://github.com/Nyr/openvpn-install/blob/master/openvpn-install.sh
 	fi
 fi
